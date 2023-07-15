e58ef6fbe8fc4ca1acb6ba4627ae7888eaabf55a
Jun Nishimura <n.junjun0303@gmail.com>
2023-07-15 20:11:17 +0900

    comment out chat logic in root (#8)

--- cmd/root.go
+++ cmd/root.go
@@ -4,12 +4,9 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"context"
 	"fmt"
 	"os"
 
-	"github.com/joho/godotenv"
-	openai "github.com/sashabaranov/go-openai"
 	"github.com/spf13/cobra"
 )
 
@@ -19,31 +16,27 @@ var rootCmd = &cobra.Command{
 	Short: "chatify is a CLI tool that suggests music recommendations for you",
 	Long:  "chatify is a CLI tool that suggests music recommendations for you",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		// load env file
-		if err := godotenv.Load(".env"); err != nil {
-			return fmt.Errorf("fail to load env file: %v", err)
-		}
-
+		fmt.Println("root called")
 		// get client
-		client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
-		resp, err := client.CreateChatCompletion(
-			context.Background(),
-			openai.ChatCompletionRequest{
-				Model: openai.GPT3Dot5Turbo,
-				Messages: []openai.ChatCompletionMessage{
-					{
-						Role:    openai.ChatMessageRoleUser,
-						Content: "Hello",
-					},
-				},
-			},
-		)
-		if err != nil {
-			return fmt.Errorf("chat completion error: %v", err)
-		}
+		// client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
+		// resp, err := client.CreateChatCompletion(
+		// 	context.Background(),
+		// 	openai.ChatCompletionRequest{
+		// 		Model: openai.GPT3Dot5Turbo,
+		// 		Messages: []openai.ChatCompletionMessage{
+		// 			{
+		// 				Role:    openai.ChatMessageRoleUser,
+		// 				Content: "Hello",
+		// 			},
+		// 		},
+		// 	},
+		// )
+		// if err != nil {
+		// 	return fmt.Errorf("chat completion error: %v", err)
+		// }
 
-		// show response
-		fmt.Println(resp.Choices[0].Message.Content)
+		// // show response
+		// fmt.Println(resp.Choices[0].Message.Content)
 
 		return nil
 	},
@@ -56,7 +49,4 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
 }
