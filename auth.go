91ddee03e4f193325b8e342122785b57e2456a13
Jun Nishimura <n.junjun0303@gmail.com>
2023-07-18 00:02:54 +0900

    add auth file (#12)

--- auth.go
+++ auth.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/google/uuid"
+	"github.com/pkg/browser"
 	"github.com/zmb3/spotify/v2"
+	spotifyauth "github.com/zmb3/spotify/v2/auth"
+)
+
+const (
+	port = "8888"
+)
+
+var (
+	auth   *spotifyauth.Authenticator
+	state  string
+	server *http.Server
 )
 
 func completeAuth(w http.ResponseWriter, r *http.Request) {
@@ -19,12 +33,40 @@ func completeAuth(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("state mismatch: got = %s, expected = %s\n", getState, state)
 	}
 
+	// set token config
+	if err := setTokenInfo(token); err != nil {
+		log.Fatal(err)
+	}
+
 	client := spotify.New(auth.Client(r.Context(), token))
 	clientChannel <- client
 
 	go func() {
 		if err := server.Shutdown(context.Background()); err != nil {
+			log.Printf("shutdown server")
 			log.Fatalf("fail to shutdown server: %v\n", err)
 		}
 	}()
+}
+
+func authorize() {
+	auth = spotifyauth.New(
+		spotifyauth.WithClientID(clientViper.GetString(SpotifyIDKeyName)),
+		spotifyauth.WithClientSecret(clientViper.GetString(SpotifySecretKeyName)),
+		spotifyauth.WithRedirectURL(redirectURI),
+		spotifyauth.WithScopes(spotifyauth.ScopeUserReadPrivate),
+	)
+	server = &http.Server{Addr: fmt.Sprintf(":%s", port)}
+	state = uuid.New().String()
+	http.HandleFunc("/callback", completeAuth)
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatal(err)
+		}
+	}()
+
+	authURL := auth.AuthURL(state)
+	if err := browser.OpenURL(authURL); err != nil {
+		log.Fatalf("fail to open: %s\n", authURL)
+	}
 }
