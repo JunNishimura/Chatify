b3ff07eed0efc15f67e63862e26ea72fa51ba5cd
Jun Nishimura <n.junjun0303@gmail.com>
2023-08-06 12:59:59 +0900

    update init (#32)

--- ui/init.go
+++ ui/init.go
@@ -1,10 +1,9 @@
 package ui
 
 import (
-	"github.com/charmbracelet/bubbles/textarea"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func (m *Model) Init() tea.Cmd {
-	return textarea.Blink
+	return m.loadConfig
 }
