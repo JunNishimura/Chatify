f6f39180f4b8a50f6a689fa147ef00162ba1be39
Jun Nishimura <n.junjun0303@gmail.com>
2023-08-16 01:57:26 +0900

    add heyUI model (#38)

---
+++ ui/hey/model.go
@@ -0,0 +1,76 @@
+package hey
+
+import (
+	"context"
+	"strings"
+
+	"github.com/JunNishimura/Chatify/ai/model"
+	"github.com/JunNishimura/Chatify/config"
+	"github.com/JunNishimura/spotify/v2"
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/sashabaranov/go-openai"
+)
+
+type sessionState uint
+
+const (
+	chatView sessionState = iota
+	recommendationView
+)
+
+func (s sessionState) String() string {
+	switch s {
+	case chatView:
+		return "chat"
+	case recommendationView:
+		return "recommendation"
+	default:
+		return ""
+	}
+}
+
+type Item struct {
+	album   string
+	artists []string
+	url     string
+}
+
+func (i Item) Title() string       { return i.album }
+func (i Item) Description() string { return strings.Join(i.artists, ", ") }
+func (i Item) FilterValue() string { return i.album }
+
+type Model struct {
+	ctx              context.Context
+	width            int
+	height           int
+	state            sessionState
+	textInput        textinput.Model
+	list             list.Model
+	cfg              *config.Config
+	user             *model.User
+	spotifyClient    *spotify.Client
+	openaiClient     *openai.Client
+	chatCompMessages []openai.ChatCompletionMessage
+	conversation     []string
+	functions        []openai.FunctionDefinition
+	availableGenres  []string
+	recommendItems   []list.Item
+}
+
+func NewModel() Model {
+	return Model{
+		ctx:       context.Background(),
+		textInput: newTextInput(),
+		list:      list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0),
+	}
+}
+
+func newTextInput() textinput.Model {
+	ti := textinput.New()
+	ti.Focus()
+	ti.CharLimit = 100
+	ti.Width = 100
+
+	return ti
+}
