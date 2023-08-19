a5471a6a142db480c79ec7ca7c9d7802e6b93ecd
Jun Nishimura <n.junjun0303@gmail.com>
2023-08-19 19:54:43 +0900

    make style variable to function (#48)

--- ui/hey/style/style.go
+++ ui/hey/style/style.go
@@ -3,15 +3,21 @@ package style
 import "github.com/charmbracelet/lipgloss"
 
 var (
-	Normal = lipgloss.NewStyle().
-		Width(100).
-		Height(40).
-		BorderStyle(lipgloss.HiddenBorder())
-	Focused = lipgloss.NewStyle().
-		Width(100).
-		Height(40).
-		BorderStyle(lipgloss.NormalBorder()).
-		BorderForeground(lipgloss.Color("69"))
 	List = lipgloss.NewStyle().
 		Margin(1, 2)
 )
+
+func GetNormal(width, height int) lipgloss.Style {
+	return lipgloss.NewStyle().
+		Width(width).
+		Height(height).
+		BorderStyle(lipgloss.HiddenBorder())
+}
+
+func GetFocused(width, height int) lipgloss.Style {
+	return lipgloss.NewStyle().
+		Width(width).
+		Height(height).
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("69"))
+}
