d02d9678dd178b561847be98ec17d51af3422cce
Jun Nishimura <n.junjun0303@gmail.com>
2023-08-20 18:08:51 +0900

    fix styling (#50)

--- ui/hey/style/style.go
+++ ui/hey/style/style.go
@@ -2,11 +2,6 @@ package style
 
 import "github.com/charmbracelet/lipgloss"
 
-var (
-	List = lipgloss.NewStyle().
-		Margin(1, 2)
-)
-
 func GetNormal(width, height int) lipgloss.Style {
 	return lipgloss.NewStyle().
 		Width(width).
