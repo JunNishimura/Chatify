9f61b2b2b24308fe1adf3cbaf917addfb8aca656
Jun Nishimura <n.junjun0303@gmail.com>
2023-08-25 01:29:19 +0900

    update model (#65)

--- ai/model/user.go
+++ ai/model/user.go
@@ -26,13 +26,23 @@ const (
 	DanceabilityElement
 	ValenceElement
 	PopularityElement
+	AcousticnessElement
+	EnergyElement
+	InstrumentalnessElement
+	LivenessElement
+	SpeechinessElement
 )
 
 type MusicOrientation struct {
-	Genres       utils.Info[[]string]
-	Danceability utils.Info[float64]
-	Valence      utils.Info[float64]
-	Popularity   utils.Info[int]
+	Genres           utils.Info[[]string]
+	Danceability     utils.Info[float64]
+	Valence          utils.Info[float64]
+	Popularity       utils.Info[int]
+	Acousticness     utils.Info[float64]
+	Energy           utils.Info[float64]
+	Instrumentalness utils.Info[float64]
+	Liveness         utils.Info[float64]
+	Speechiness      utils.Info[float64]
 }
 
 func newMusicOrientation() *MusicOrientation {
@@ -53,6 +63,26 @@ func newMusicOrientation() *MusicOrientation {
 			Value:      0,
 			HasChanged: false,
 		},
+		Acousticness: utils.Info[float64]{
+			Value:      0.0,
+			HasChanged: false,
+		},
+		Energy: utils.Info[float64]{
+			Value:      0.0,
+			HasChanged: false,
+		},
+		Instrumentalness: utils.Info[float64]{
+			Value:      0.0,
+			HasChanged: false,
+		},
+		Liveness: utils.Info[float64]{
+			Value:      0.0,
+			HasChanged: false,
+		},
+		Speechiness: utils.Info[float64]{
+			Value:      0.0,
+			HasChanged: false,
+		},
 	}
 }
 
@@ -62,6 +92,11 @@ func (m *MusicOrientation) String() string {
 	output += fmt.Sprintf("danceability: %f\n", m.Danceability.Value)
 	output += fmt.Sprintf("valence: %f\n", m.Valence.Value)
 	output += fmt.Sprintf("popularity: %d\n", m.Popularity.Value)
+	output += fmt.Sprintf("acousticness: %f\n", m.Acousticness.Value)
+	output += fmt.Sprintf("energy: %f\n", m.Energy.Value)
+	output += fmt.Sprintf("instrumentalness: %f\n", m.Instrumentalness.Value)
+	output += fmt.Sprintf("liveness: %f\n", m.Liveness.Value)
+	output += fmt.Sprintf("speechiness: %f\n", m.Speechiness.Value)
 	return output
 }
 
@@ -69,5 +104,10 @@ func (m *MusicOrientation) HasAllSet() bool {
 	return m.Genres.HasChanged &&
 		m.Danceability.HasChanged &&
 		m.Valence.HasChanged &&
-		m.Popularity.HasChanged
+		m.Popularity.HasChanged &&
+		m.Acousticness.HasChanged &&
+		m.Energy.HasChanged &&
+		m.Instrumentalness.HasChanged &&
+		m.Liveness.HasChanged &&
+		m.Speechiness.HasChanged
 }
