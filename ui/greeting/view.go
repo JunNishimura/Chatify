fd02cb3b56312c7f622fbfdf124b024aa0781b39
Jun Nishimura <n.junjun0303@gmail.com>
2023-08-27 20:31:52 +0900

    update view (#67)

--- ui/greeting/view.go
+++ ui/greeting/view.go
@@ -3,40 +3,61 @@ package greeting
 import (
 	"fmt"
 
+	"github.com/JunNishimura/Chatify/ui/style"
 	"github.com/charmbracelet/lipgloss"
 )
 
-var (
-	checkMark = lipgloss.NewStyle().Foreground(lipgloss.Color("42")).SetString("✓")
-	doneStyle = lipgloss.NewStyle().Margin(1, 2)
-)
+func (m *Model) View() string {
+	return lipgloss.Place(m.window.Width, m.window.Height, lipgloss.Center, lipgloss.Center,
+		lipgloss.JoinVertical(
+			lipgloss.Center,
+			style.AsciiArt().Render(asciiArtView),
+			style.ChatFocused(m.getViewWidth(), m.getViewHeight()).Render(m.chatView()),
+		),
+	)
+}
 
-func (m Model) View() string {
-	view := m.viewport.View()
-	text := m.textarea.View()
+func (m *Model) getViewWidth() int {
+	halfWidth := m.window.Width / 2
+	return halfWidth
+}
 
-	var output string
-	if m.greetingDone {
-		output += doneStyle.Render(fmt.Sprintf(`Nice to see you, %s!!
-	
-If you want to talk to me, please type
+func (m *Model) getViewHeight() int {
+	halfHeight := m.window.Height * 2 / 3
+	return halfHeight
+}
 
-  $ chatify hey
+var asciiArtView = `
+  ___  _   _    __   ____  ____  ____  _  _
+ / __)( )_( )  /__\ (_  _)(_  _)( ___)( \/ )
+( (__  ) _ (  /(__)\  )(   _)(_  )__)  \  /
+ \___)(_) (_)(__)(__)(__) (____)(__)   (__)
+ `
 
-See you then!! (press to "enter" to exit)`,
-			m.user.DisplayName,
-		))
-	} else if m.qaDone {
-		for i, key := range m.confKeyList[:m.writeIndex] {
-			output += fmt.Sprintf("%s set %s as %s\n", checkMark, key, m.qaList[i].answer)
+func (m *Model) chatView() string {
+	var s string
+	switch m.phase {
+	case completePhase:
+		var ss string
+		ss += lipgloss.NewStyle().Width(m.getViewWidth()).Align(lipgloss.Center, lipgloss.Center).Render(fmt.Sprintf("Nice to see you, %s!", m.user.DisplayName)) + "\n\n"
+		ss += lipgloss.NewStyle().Width(m.getViewWidth()).Align(lipgloss.Center, lipgloss.Center).Render("If you want to talk to me, please type") + "\n\n"
+		ss += lipgloss.NewStyle().Width(m.getViewWidth()).Align(lipgloss.Center, lipgloss.Center).Foreground(lipgloss.Color(style.HighlightColor)).Background(lipgloss.Color(style.BgColor)).Render("$ chatify hey") + "\n\n"
+		ss += lipgloss.NewStyle().Width(m.getViewWidth()).Align(lipgloss.Center, lipgloss.Center).Render(`See you then!! (press to "enter" to exit)`)
+		s = lipgloss.Place(m.getViewWidth(), m.getViewHeight(), lipgloss.Center, lipgloss.Center, ss)
+	default:
+		var lastSpeaker Speaker
+		for _, message := range m.conversation {
+			if message.speaker == Bot {
+				s += style.BotChat(m.getViewWidth()).Render(message.content) + "\n\n"
+				lastSpeaker = Bot
+			} else if message.speaker == User {
+				s += style.UserChat().Render(message.content) + "\n\n"
+				lastSpeaker = User
+			}
 		}
-
-		if m.setConfigDone {
-			output += doneStyle.Render(`Press "enter" to authorize`)
+		if s != "" && lastSpeaker == Bot && m.phase == questionPhase {
+			s += style.TextInput().Render(m.textInput.View())
 		}
-	} else {
-		output = fmt.Sprintf("%s\n\n%s\n\n", view, text)
 	}
-
-	return output
+	return s
 }
