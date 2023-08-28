30e16bd2adead80f16b18e7c8801b5ad54a6c10e
Jun Nishimura <n.junjun0303@gmail.com>
2023-08-29 01:06:52 +0900

    add base model (#86)

---
+++ ui/cmd/base/model.go
@@ -0,0 +1,63 @@
+package base
+
+import (
+	"github.com/JunNishimura/Chatify/config"
+	"github.com/JunNishimura/Chatify/utils"
+	"github.com/charmbracelet/bubbles/textinput"
+)
+
+type Speaker int
+
+const (
+	Bot Speaker = iota
+	User
+)
+
+type Message struct {
+	Content string
+	Speaker Speaker
+}
+
+type Model struct {
+	Window       *utils.Window
+	Cfg          *config.Config
+	TextInput    textinput.Model
+	Conversation []*Message
+}
+
+func NewModel() (*Model, error) {
+	window := utils.NewWindow()
+
+	cfg, err := loadConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Model{
+		Window:    window,
+		Cfg:       cfg,
+		TextInput: newTextInput(window.Width),
+	}, nil
+}
+
+func loadConfig() (*config.Config, error) {
+	cfg, err := config.New()
+	if err != nil {
+		return nil, err
+	}
+
+	if err := cfg.Load(); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+func newTextInput(width int) textinput.Model {
+	ti := textinput.New()
+	ti.Focus()
+	ti.CharLimit = 100
+	ti.Width = width
+
+	return ti
+}
